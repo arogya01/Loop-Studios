9777f7b7e872b311a7da9c2dbb8cf15d58e7397a
arogya <arogyabichpuria16@gmail.com>
2021-05-15 16:19:57 +0530

    pre-final version ready

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-let ham=document.querySelector(".ham");
 
-ham.addEventListener("onclick",(e)=>{
- 
+window.addEventListener("resize",()=>{
+    
+    //when the width is less than 1000px, set .ham->visible;
+     if(window.innerWidth<=1000){
+   document.getElementById("ham").style.display="initial";
+   document.querySelector(".navbar").style.display="none";
+   document.getElementById("ham").addEventListener("click",()=>{
+    console.log("woe");
+    document.querySelector(".slogan").style.display="none";
+    document.querySelector(".heroSection").style.background="black";
+    document.getElementById("ham").style.display="none";
+    document.getElementById("close").style.display="initial";
+    document.querySelector(".navbar").style.display="initial";
+
+    document.getElementById("close").addEventListener("click",()=>{
+        document.getElementById("ham").style.display="initial";
+        document.getElementById("close").style.display="none";
+        document.querySelector(".heroSection").style.removeProperty("background");
+        document.querySelector(".slogan").style.display="initial";
+        document.querySelector(".navbar").style.display="none";
+    });
 });
+     }
+     else if(window.innerWidth>1000){
+         document.getElementById("ham").style.display="none";
+         document.querySelector(".navbar").style.display="initial";
+     }
+});
+
+
