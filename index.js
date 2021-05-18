4ee16c7e1a0c4d4bd791c6f4ea840b37b47e09c6
arogya bichpuria <arogyabichpuria16@gmail.com>
2021-05-18 15:44:37 +0530

    all minor bugs resolved

--- index.js
+++ index.js
@@ -1,41 +1,53 @@
 
-if(window.innerWidth<=1000){ 
-    document.getElementById("ham").style.display="initial";
-    document.querySelector(".navbar").style.display="none";
-}
-if(window.innerWidth>1000){ 
-    document.getElementById("ham").style.display="none";
-}
+checkWidth();
 
+window.addEventListener("resize", () => {
 
-window.addEventListener("resize",()=>{
-    
     //when the width is less than 1000px, set .ham->visible;
-     if(window.innerWidth<=1000){
-   document.getElementById("ham").style.display="initial";
-   document.querySelector(".navbar").style.display="none";
-   
-   document.getElementById("ham").addEventListener("click",()=>{
-    console.log("woe");
-    document.querySelector(".slogan").style.display="none";
-    document.querySelector(".heroSection").style.background="black";
-    document.getElementById("ham").style.display="none";
-    document.getElementById("close").style.display="initial";
-    document.querySelector(".navbar").style.display="initial";
+   checkWidth();
+});
 
-    document.getElementById("close").addEventListener("click",()=>{
-        document.getElementById("ham").style.display="initial";
-        document.getElementById("close").style.display="none";
-        document.querySelector(".heroSection").style.removeProperty("background");
-        document.querySelector(".slogan").style.display="initial";
-        document.querySelector(".navbar").style.display="none";
+function checkWidth(){
+
+    if (window.innerWidth <= 1000) {
+        document.getElementById("ham").style.display = "initial";
+        document.querySelector(".navbar").style.display = "none";
+
+        apply_EventListener_ham();
+
+
+    }
+    else if (window.innerWidth > 1000) {
+        document.getElementById("ham").style.display = "none";
+        document.querySelector(".navbar").style.display = "initial";
+    }
+
+}
+
+function apply_EventListener_ham() {
+
+
+    document.getElementById("ham").addEventListener("click", () => {
+        console.log("woe");
+        document.querySelector(".slogan").style.display = "none";
+        document.querySelector(".heroSection").style.background = "black";
+        document.getElementById("ham").style.display = "none";
+        document.getElementById("close").style.display = "initial";
+        document.querySelector(".navbar").style.display = "initial";
+
+        apply_EventListener_Close();
     });
-});
-     }
-     else if(window.innerWidth>1000){
-         document.getElementById("ham").style.display="none";
-         document.querySelector(".navbar").style.display="initial";
-     }
-});
+}
 
 
+
+function apply_EventListener_Close(){
+
+    document.getElementById("close").addEventListener("click", () => {
+        document.getElementById("ham").style.display = "initial";
+        document.getElementById("close").style.display = "none";
+        document.querySelector(".heroSection").style.removeProperty("background");
+        document.querySelector(".slogan").style.display = "initial";
+        document.querySelector(".navbar").style.display = "none";
+    });
+}
